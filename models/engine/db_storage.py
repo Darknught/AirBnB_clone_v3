383c028844fc3c853c3a6acaf5936c200b7ce440
Pibo Shongwe <piboman@yahoo.com>
2024-03-31 12:01:07 -0700

    task2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve one object based on the class and its ID"""
+        if cls in classes.values() and id and type(id) == str:
+            obj = self.all(cls)
+            for k, v in obj.items():
+                if k.split(".")[1] == id:
+                    return v
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        cnt = 0
+        cls_dict = self.all(cls)
+        cnt = len(cls_dict)
+        return cnt
