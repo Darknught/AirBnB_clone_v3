deb61168b9b9f7254c68628c6b1c9c6f12b1c502
Pibo Shongwe <piboman@yahoo.com>
2024-04-01 05:23:50 -0700

    task13

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
 if app_views is not None:
+    from .places_amenities import *
     from .places_reviews import *
     from .places import *
     from .users import *
