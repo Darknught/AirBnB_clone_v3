c684783d80094682c9f002f95b6315174fcd4a60
Pibo Shongwe <piboman@yahoo.com>
2024-04-01 00:06:14 -0700

    u

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,11 @@ from flask import Blueprint
 
 app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 
-from .places_reviews import *
-from .places import *
-from .users import *
-from .amenities import *
-from .cities import *
-from .states import *
-from .index import *
+if app_views is not None:
+    from .places_reviews import *
+    from .places import *
+    from .users import *
+    from .amenities import *
+    from .cities import *
+    from .states import *
+    from .index import *
