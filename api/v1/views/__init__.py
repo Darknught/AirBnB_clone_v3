d604ad5b3e0a21ad915ddcd5f2bb2645b07bca8e
Pibo Shongwe <piboman@yahoo.com>
2024-03-31 23:25:22 -0700

    task9

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Init file"""
 from flask import Blueprint
-
-app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
-
 from .index import *
 from .states import *
 from .cities import *
-from .amenities import * 
+from .amenities import *
+from .users import *
+
+app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
